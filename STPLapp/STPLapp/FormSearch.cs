56cbb1d15ff47eb556e4397fe98037d2aed5a750
Nafa06 <naufalanggardi09@gmail.com>
2026-04-17 22:04:48 +0700

    ADD: Menambahkan tampilan 3 page baru(Menu, Input Page, Search Page), Menambahkan textbox, label, dan button di setiap page
    UPDATE: Mengubah form1 menjadi login page

--- STPLapp/STPLapp/FormSearch.cs
+++ STPLapp/STPLapp/FormSearch.cs
@@ -10,14 +10,14 @@ using System.Windows.Forms;
 
 namespace STPLapp
 {
-    public partial class Form1 : Form
+    public partial class FormSearch : Form
     {
-        public Form1()
+        public FormSearch()
         {
             InitializeComponent();
         }
 
-        private void Form1_Load(object sender, EventArgs e)
+        private void FormSearch_Load(object sender, EventArgs e)
         {
 
         }
