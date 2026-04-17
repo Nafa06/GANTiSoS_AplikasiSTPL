f4c7da5b208acac46e1e5ea5aa7d5d4a8d43ebc1
Nafa06 <naufalanggardi09@gmail.com>
2026-04-18 02:23:12 +0700

    UPDATE: Mengubah tampilan search page
    ADD: Menambahkan nama design pada setiap label dan btn di search page

--- STPLapp/STPLapp/FormSearch.Designer.cs
+++ STPLapp/STPLapp/FormSearch.Designer.cs
@@ -29,87 +29,299 @@
         private void InitializeComponent()
         {
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.button3 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button5 = new System.Windows.Forms.Button();
-            this.textBox8 = new System.Windows.Forms.TextBox();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button6 = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.txtSearch = new System.Windows.Forms.TextBox();
+            this.btnBack = new System.Windows.Forms.Button();
+            this.btnRefresh = new System.Windows.Forms.Button();
+            this.cmbKategori = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtTkp = new System.Windows.Forms.TextBox();
+            this.label15 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtCiri = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtJenis = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.lblNik = new System.Windows.Forms.Label();
+            this.lblNama = new System.Windows.Forms.Label();
+            this.lblId = new System.Windows.Forms.Label();
+            this.lblTanggal = new System.Windows.Forms.Label();
+            this.lblNrp = new System.Windows.Forms.Label();
+            this.cmbStatus = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // dataGridView1
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(45, 94);
+            this.dataGridView1.Location = new System.Drawing.Point(19, 81);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowHeadersWidth = 51;
             this.dataGridView1.RowTemplate.Height = 24;
-            this.dataGridView1.Size = new System.Drawing.Size(590, 380);
+            this.dataGridView1.Size = new System.Drawing.Size(608, 412);
             this.dataGridView1.TabIndex = 10;
             // 
-            // button3
+            // btnDelete
             // 
-            this.button3.Location = new System.Drawing.Point(273, 57);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(110, 23);
-            this.button3.TabIndex = 6;
-            this.button3.Text = "Hapus";
-            this.button3.UseVisualStyleBackColor = true;
+            this.btnDelete.Location = new System.Drawing.Point(747, 49);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(110, 23);
+            this.btnDelete.TabIndex = 6;
+            this.btnDelete.Text = "Hapus";
+            this.btnDelete.UseVisualStyleBackColor = true;
             // 
-            // button2
+            // btnUpdate
             // 
-            this.button2.Location = new System.Drawing.Point(158, 57);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(110, 23);
-            this.button2.TabIndex = 7;
-            this.button2.Text = "Ubah";
-            this.button2.UseVisualStyleBackColor = true;
+            this.btnUpdate.Location = new System.Drawing.Point(632, 49);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(110, 23);
+            this.btnUpdate.TabIndex = 7;
+            this.btnUpdate.Text = "Ubah";
+            this.btnUpdate.UseVisualStyleBackColor = true;
             // 
-            // button5
+            // btnSearch
             // 
-            this.button5.Location = new System.Drawing.Point(274, 27);
-            this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(109, 23);
-            this.button5.TabIndex = 8;
-            this.button5.Text = "Cari No STPL";
-            this.button5.UseVisualStyleBackColor = true;
+            this.btnSearch.Location = new System.Drawing.Point(387, 48);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(109, 23);
+            this.btnSearch.TabIndex = 8;
+            this.btnSearch.Text = "Cari No STPL";
+            this.btnSearch.UseVisualStyleBackColor = true;
             // 
-            // textBox8
+            // txtSearch
             // 
-            this.textBox8.Location = new System.Drawing.Point(45, 28);
-            this.textBox8.Name = "textBox8";
-            this.textBox8.Size = new System.Drawing.Size(223, 22);
-            this.textBox8.TabIndex = 5;
+            this.txtSearch.Location = new System.Drawing.Point(155, 49);
+            this.txtSearch.Name = "txtSearch";
+            this.txtSearch.Size = new System.Drawing.Size(226, 22);
+            this.txtSearch.TabIndex = 5;
             // 
-            // button1
+            // btnBack
             // 
-            this.button1.Location = new System.Drawing.Point(525, 487);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(110, 23);
-            this.button1.TabIndex = 6;
-            this.button1.Text = "Kembali";
-            this.button1.UseVisualStyleBackColor = true;
+            this.btnBack.Location = new System.Drawing.Point(879, 498);
+            this.btnBack.Name = "btnBack";
+            this.btnBack.Size = new System.Drawing.Size(110, 23);
+            this.btnBack.TabIndex = 6;
+            this.btnBack.Text = "Kembali";
+            this.btnBack.UseVisualStyleBackColor = true;
             // 
-            // button6
+            // btnRefresh
             // 
-            this.button6.Location = new System.Drawing.Point(42, 57);
-            this.button6.Name = "button6";
-            this.button6.Size = new System.Drawing.Size(110, 23);
-            this.button6.TabIndex = 6;
-            this.button6.Text = "Refresh";
-            this.button6.UseVisualStyleBackColor = true;
+            this.btnRefresh.Location = new System.Drawing.Point(516, 49);
+            this.btnRefresh.Name = "btnRefresh";
+            this.btnRefresh.Size = new System.Drawing.Size(110, 23);
+            this.btnRefresh.TabIndex = 6;
+            this.btnRefresh.Text = "Refresh";
+            this.btnRefresh.UseVisualStyleBackColor = true;
+            // 
+            // cmbKategori
+            // 
+            this.cmbKategori.FormattingEnabled = true;
+            this.cmbKategori.Items.AddRange(new object[] {
+            "Laporan Hilang",
+            "Barang Temuan"});
+            this.cmbKategori.Location = new System.Drawing.Point(19, 47);
+            this.cmbKategori.Name = "cmbKategori";
+            this.cmbKategori.Size = new System.Drawing.Size(130, 24);
+            this.cmbKategori.TabIndex = 11;
+            this.cmbKategori.Text = "Jenis Laporan";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(638, 84);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(62, 16);
+            this.label3.TabIndex = 25;
+            this.label3.Text = "No STPL";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(638, 224);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(72, 16);
+            this.label5.TabIndex = 23;
+            this.label5.Text = "Ciri Khusus";
+            // 
+            // txtTkp
+            // 
+            this.txtTkp.Location = new System.Drawing.Point(747, 306);
+            this.txtTkp.Multiline = true;
+            this.txtTkp.Name = "txtTkp";
+            this.txtTkp.Size = new System.Drawing.Size(190, 57);
+            this.txtTkp.TabIndex = 24;
+            // 
+            // label15
+            // 
+            this.label15.AutoSize = true;
+            this.label15.Location = new System.Drawing.Point(638, 168);
+            this.label15.Name = "label15";
+            this.label15.Size = new System.Drawing.Size(86, 16);
+            this.label15.TabIndex = 20;
+            this.label15.Text = "Jenis Barang";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(638, 140);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(95, 16);
+            this.label4.TabIndex = 21;
+            this.label4.Text = "Nama Pelapor";
+            // 
+            // txtCiri
+            // 
+            this.txtCiri.Location = new System.Drawing.Point(747, 221);
+            this.txtCiri.Multiline = true;
+            this.txtCiri.Name = "txtCiri";
+            this.txtCiri.Size = new System.Drawing.Size(190, 79);
+            this.txtCiri.TabIndex = 22;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(638, 372);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(89, 16);
+            this.label6.TabIndex = 19;
+            this.label6.Text = "NRP Petugas";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(638, 112);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(79, 16);
+            this.label7.TabIndex = 29;
+            this.label7.Text = "NIK Pelapor";
+            // 
+            // txtJenis
+            // 
+            this.txtJenis.Location = new System.Drawing.Point(747, 165);
+            this.txtJenis.Name = "txtJenis";
+            this.txtJenis.Size = new System.Drawing.Size(190, 22);
+            this.txtJenis.TabIndex = 17;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(638, 309);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(33, 16);
+            this.label8.TabIndex = 28;
+            this.label8.Text = "TKP";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(638, 196);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(58, 16);
+            this.label9.TabIndex = 27;
+            this.label9.Text = "Tanggal";
+            // 
+            // lblNik
+            // 
+            this.lblNik.AutoSize = true;
+            this.lblNik.Location = new System.Drawing.Point(744, 112);
+            this.lblNik.Name = "lblNik";
+            this.lblNik.Size = new System.Drawing.Size(11, 16);
+            this.lblNik.TabIndex = 29;
+            this.lblNik.Text = "-";
+            // 
+            // lblNama
+            // 
+            this.lblNama.AutoSize = true;
+            this.lblNama.Location = new System.Drawing.Point(744, 140);
+            this.lblNama.Name = "lblNama";
+            this.lblNama.Size = new System.Drawing.Size(11, 16);
+            this.lblNama.TabIndex = 21;
+            this.lblNama.Text = "-";
+            // 
+            // lblId
+            // 
+            this.lblId.AutoSize = true;
+            this.lblId.Location = new System.Drawing.Point(744, 84);
+            this.lblId.Name = "lblId";
+            this.lblId.Size = new System.Drawing.Size(11, 16);
+            this.lblId.TabIndex = 25;
+            this.lblId.Text = "-";
+            // 
+            // lblTanggal
+            // 
+            this.lblTanggal.AutoSize = true;
+            this.lblTanggal.Location = new System.Drawing.Point(744, 196);
+            this.lblTanggal.Name = "lblTanggal";
+            this.lblTanggal.Size = new System.Drawing.Size(11, 16);
+            this.lblTanggal.TabIndex = 27;
+            this.lblTanggal.Text = "-";
+            // 
+            // lblNrp
+            // 
+            this.lblNrp.AutoSize = true;
+            this.lblNrp.Location = new System.Drawing.Point(744, 372);
+            this.lblNrp.Name = "lblNrp";
+            this.lblNrp.Size = new System.Drawing.Size(11, 16);
+            this.lblNrp.TabIndex = 19;
+            this.lblNrp.Text = "-";
+            // 
+            // cmbStatus
+            // 
+            this.cmbStatus.FormattingEnabled = true;
+            this.cmbStatus.Items.AddRange(new object[] {
+            "Dicari",
+            "Tersimpan",
+            "Dikembalikan"});
+            this.cmbStatus.Location = new System.Drawing.Point(747, 396);
+            this.cmbStatus.Name = "cmbStatus";
+            this.cmbStatus.Size = new System.Drawing.Size(121, 24);
+            this.cmbStatus.TabIndex = 11;
+            this.cmbStatus.Text = "Status Barang";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(638, 399);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(91, 16);
+            this.label2.TabIndex = 20;
+            this.label2.Text = "Status Barang";
             // 
             // FormSearch
             // 
-            this.ClientSize = new System.Drawing.Size(691, 529);
+            this.ClientSize = new System.Drawing.Size(1001, 529);
+            this.Controls.Add(this.lblId);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtTkp);
+            this.Controls.Add(this.lblNama);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label15);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtCiri);
+            this.Controls.Add(this.lblNik);
+            this.Controls.Add(this.lblNrp);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.txtJenis);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.lblTanggal);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.cmbStatus);
+            this.Controls.Add(this.cmbKategori);
             this.Controls.Add(this.dataGridView1);
-            this.Controls.Add(this.button6);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button5);
-            this.Controls.Add(this.textBox8);
+            this.Controls.Add(this.btnRefresh);
+            this.Controls.Add(this.btnBack);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.txtSearch);
             this.Name = "FormSearch";
             this.Text = "Search Page";
             this.Load += new System.EventHandler(this.FormSearch_Load);
@@ -122,11 +334,30 @@
         #endregion
 
         private System.Windows.Forms.DataGridView dataGridView1;
-        private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button5;
-        private System.Windows.Forms.TextBox textBox8;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.TextBox txtSearch;
+        private System.Windows.Forms.Button btnBack;
+        private System.Windows.Forms.Button btnRefresh;
+        private System.Windows.Forms.ComboBox cmbKategori;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtTkp;
+        private System.Windows.Forms.Label label15;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtCiri;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtJenis;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label lblNik;
+        private System.Windows.Forms.Label lblNama;
+        private System.Windows.Forms.Label lblId;
+        private System.Windows.Forms.Label lblTanggal;
+        private System.Windows.Forms.Label lblNrp;
+        private System.Windows.Forms.ComboBox cmbStatus;
+        private System.Windows.Forms.Label label2;
     }
 }
