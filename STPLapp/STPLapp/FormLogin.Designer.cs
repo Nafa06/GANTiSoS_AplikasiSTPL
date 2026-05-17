6fe5f3daf4bab0bdeedf51305320a6abec9de12c
Nafa06 <naufalanggardi09@gmail.com>
2026-05-18 05:50:02 +0700

    ADD: Menambahkan logo dan teks app pada formLogin

--- STPLapp/STPLapp/FormLogin.Designer.cs
+++ STPLapp/STPLapp/FormLogin.Designer.cs
@@ -34,11 +34,15 @@
             this.label = new System.Windows.Forms.Label();
             this.btnLogin = new System.Windows.Forms.Button();
             this.btnConnect = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // txtPass
             // 
-            this.txtPass.Location = new System.Drawing.Point(207, 194);
+            this.txtPass.Location = new System.Drawing.Point(204, 268);
             this.txtPass.Name = "txtPass";
             this.txtPass.PasswordChar = '*';
             this.txtPass.Size = new System.Drawing.Size(190, 22);
@@ -46,7 +50,7 @@
             // 
             // txtNrp
             // 
-            this.txtNrp.Location = new System.Drawing.Point(207, 166);
+            this.txtNrp.Location = new System.Drawing.Point(204, 240);
             this.txtNrp.Name = "txtNrp";
             this.txtNrp.Size = new System.Drawing.Size(190, 22);
             this.txtNrp.TabIndex = 0;
@@ -54,7 +58,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(133, 197);
+            this.label2.Location = new System.Drawing.Point(130, 271);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(67, 16);
             this.label2.TabIndex = 19;
@@ -63,7 +67,7 @@
             // label
             // 
             this.label.AutoSize = true;
-            this.label.Location = new System.Drawing.Point(133, 169);
+            this.label.Location = new System.Drawing.Point(130, 243);
             this.label.Name = "label";
             this.label.Size = new System.Drawing.Size(36, 16);
             this.label.TabIndex = 18;
@@ -71,7 +75,7 @@
             // 
             // btnLogin
             // 
-            this.btnLogin.Location = new System.Drawing.Point(267, 222);
+            this.btnLogin.Location = new System.Drawing.Point(264, 296);
             this.btnLogin.Name = "btnLogin";
             this.btnLogin.Size = new System.Drawing.Size(130, 34);
             this.btnLogin.TabIndex = 3;
@@ -81,7 +85,7 @@
             // 
             // btnConnect
             // 
-            this.btnConnect.Location = new System.Drawing.Point(136, 222);
+            this.btnConnect.Location = new System.Drawing.Point(133, 296);
             this.btnConnect.Name = "btnConnect";
             this.btnConnect.Size = new System.Drawing.Size(130, 34);
             this.btnConnect.TabIndex = 2;
@@ -89,11 +93,44 @@
             this.btnConnect.UseVisualStyleBackColor = true;
             this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
             // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::STPLapp.Properties.Resources.PolisiJogjaLogo;
+            this.pictureBox1.Location = new System.Drawing.Point(196, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(147, 179);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 20;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(165, 194);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(212, 16);
+            this.label3.TabIndex = 22;
+            this.label3.Text = "SISTEM LAPORAN KEHILANGAN";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(172, 210);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(195, 16);
+            this.label1.TabIndex = 22;
+            this.label1.Text = "KEPOLISIAN DI YOGYAKARTA";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
             // FormLogin
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(538, 345);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.btnConnect);
             this.Controls.Add(this.btnLogin);
             this.Controls.Add(this.txtPass);
@@ -104,6 +141,7 @@
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Login Page";
             this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -117,6 +155,9 @@
         private System.Windows.Forms.Label label;
         private System.Windows.Forms.Button btnLogin;
         private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label1;
     }
 }
 
